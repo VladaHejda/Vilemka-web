baf4e325e43d4fa5686fea3c89c6b358fa668fd6
VladaHejda <hejdav@centrum.cz>
2014-06-18 20:06:56 +0200

    second block

--- js/animations.js
+++ js/animations.js
@@ -2,9 +2,29 @@ $(function() {
 
 	var win = $(window);
 
-	var infoBlock = $('#info');
-	win.on('scroll', function(){
-		var move = 290 + win.scrollTop();
-		infoBlock.css('top', '-' + move + 'px');
+	var blocks = {
+		info: 290,
+		photo: 658
+	};
+	var top, limit, move;
+
+	var placeBlock = function(block, basePosition) {
+		top = win.scrollTop();
+		limit = basePosition + 10;
+		if (top <= limit) {
+			move = basePosition + top;
+		} else {
+			move = basePosition + limit;
+		}
+		block.css('top', '-' + move + 'px');
+	};
+
+	$.each(blocks, function(id, basePosition) {
+		placeBlock($('#' + id), basePosition);
+	});
+
+	win.on('scroll', function(event){
+		placeBlock($('#info'), 290);
+		placeBlock($('#photo'), 658);
 	});
 });
