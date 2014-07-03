4e5c33e0af587c5cb4e23178499beb5e1139200e
VladaHejda <hejdav@centrum.cz>
2014-07-03 19:47:29 +0200

    js fixed vars

--- js/animations.js
+++ js/animations.js
@@ -3,16 +3,16 @@ $(function() {
 	var slowdown = 1.5;
 
 	var win = $(window);
-	// todo why are not callback locale?
-	var scroll, move, distance, source;
+
 	++slowdown;
 	var blocks = [
 		$('header'), $('#info'), $('#photo'), $('#occupancy'), $('#reservation'), $('#contact')
 	];
 	win.on('scroll', function(event){
-		scroll = win.scrollTop();
+		var scroll = win.scrollTop();
 		distance = 0;
 
+		var move, distance, source;
 		for (var i = 0; i < blocks.length; ++i) {
 			if (i) {
 				source = i == 1 ? blocks[i-1] : $('.content', blocks[i-1]);
